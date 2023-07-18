9b6eb9ed38db42ec7b7dee1aeafdae2b821c864b
Harsha Maria Jose <harshamariajose.20@gmail.com>
2023-07-18 16:06:53 +0530

    Done

--- Objects/3.js
+++ Objects/3.js
@@ -1,16 +1,16 @@
 const phoneBook = {
-    'Anna': '6224486020',
-    'Sera': '7890123456',
-    'Nakshathra': '7845961224',
-    'Sania': '6224846896'
-  };
-  function findPhoneNumber(name) {
-    if (name in phoneBook) {
-      return phoneBook[name];
-    } else {
-      return 'Phone number not found.';
-    }
+  'Anna': '6224486020',
+  'Sera': '7890123456',
+  'Nakshathra': '7845961224',
+  'Sania': '6224846896'
+};
+function findPhoneNumber(name) {
+  if (phoneBook.hasOwnProperty(name)) {
+    return phoneBook[name];
+  } else {
+    return 'Phone number not found.';
   }
-  const name = 'Anna';
-  const phoneNumber = findPhoneNumber(name);
-  console.log(`Phone number for ${name}: ${phoneNumber}`);
+}
+const name = 'Anna';
+const phoneNumber = findPhoneNumber(name);
+console.log('Phone number for ' + name + ': ' + phoneNumber);
